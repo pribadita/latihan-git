9ceb7aca82109589b9706167b85d4713f470dab6
pribadita <pribadi.triagil@gmail.com>
2026-06-25 10:05:50 +0800

    function Finsih

---
+++ function.js
@@ -0,0 +1,33 @@
+// function tampilkan(){
+//     console.log("halo")
+// }
+// tampilkan()
+
+var nama = "Huda"
+
+
+function tampilkanNama(a){
+    console.log("halo, "+ a)
+
+}
+tampilkanNama(nama)
+
+// function munculkanAngkaDua(){
+//     return 2
+// }
+
+// var tampung = munculkanAngkaDua()
+// console.log(tampung)
+
+
+function tampilkanAngka(a, b){
+    return a + b
+}
+
+console.log(tampilkanAngka(5,2))
+
+// var tampilkanAngka = function(angkaPertama, angkaKedua){
+//     return angkaPertama + angkaKedua
+// }
+
+// console.log(tampilkanAngka(5,3))
